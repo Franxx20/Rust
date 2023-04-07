bb1ccf0ca0600ef84baac85c50c052ce5f1334f1
Franxx20 <francojg2013.Fg@gmail.com>
2023-04-07 03:48:22 -0300

    Triangulos
    Port Triangulos de C a Rust

---
+++ triangles/target/debug/deps/triangles-e2e87c2e8b8def02.d
@@ -0,0 +1,5 @@
+/home/pilum/Rust/triangles/target/debug/deps/triangles-e2e87c2e8b8def02: src/main.rs
+
+/home/pilum/Rust/triangles/target/debug/deps/triangles-e2e87c2e8b8def02.d: src/main.rs
+
+src/main.rs:
---
+++ triangles/target/debug/triangles.d
@@ -0,0 +1,1 @@
+/home/pilum/Rust/triangles/target/debug/triangles: /home/pilum/Rust/triangles/src/main.rs
